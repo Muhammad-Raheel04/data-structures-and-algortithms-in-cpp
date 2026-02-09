e5d77a4ca87e03c3fc2830b91baf1b929190f393
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-09 17:36:19 +0500

    solved question 3 of Lab 02

---
+++ Lab Codes/Lab 02/sortArraySelectionSort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={5,4,3,2,1};
+    int length=sizeof(arr)/sizeof(arr[0]);
+
+    for(int i=0;i<length-1;i++){
+        int minIndex=i;
+
+        for(int j=i+1;j<length;j++){
+            if(arr[i]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+
+        swap(arr[i],arr[minIndex]);
+    }
+
+    cout<<"Sorted Array ";
+    for(int i=0;i<length;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
