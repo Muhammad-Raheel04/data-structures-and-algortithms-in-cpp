a111d59d7daff205096578e6475a67d463bfab80
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-23 11:50:18 +0500

    added solution of Lab 04

---
+++ Lab Codes/Lab 04/solutions/linkedList_intro.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node* next;
+};
+int main(){
+    // Create Three Nodes
+    Node* head= nullptr;
+    Node* first=new Node();
+    Node* second=new Node();
+    
+    // Assign data and next points
+    first->data=20;
+    first->next=second;
+
+    
+    second->data=30;
+    second->next=nullptr;
+
+    // Traverse and print list
+    Node* temp=head;
+    while(temp!=nullptr){
+        cout<<temp->data<<" -> ";
+        temp=temp->next;
+    }
+    cout<<"NULL"<<endl;
+    return 0;
+}
